c79f3babb7e9b18001c5f1fe1e8f8a44f743bdff
anurag tiwari <iotbyanurag@gmail.com>
2020-12-26 10:16:14 +0530

    This is a new commit for what I originally planned to be amended

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+function myFirstFunction(){
+
+    echo 2+2;
+    echo "<h1> Hi Bro</h1>";
+}
+myFirstFunction();
+?>
+
+<h1><?php bloginfo('name');?></h1>
+
+<p><?php bloginfo('description');?> </p>
