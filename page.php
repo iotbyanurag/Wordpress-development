4cf9b926b3842298dac83152b4dcd8e9a85155eb
anurag tiwari <iotbyanurag@gmail.com>
2021-02-03 21:46:07 +0530

    updated menu tab

--- page.php
+++ page.php
@@ -18,19 +18,6 @@ get_header();
 
 
 <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
-<aside id="colorlib-aside" role="complementary" class="js-fullheight">
-    <nav id="colorlib-main-menu" role="navigation">
-        <ul>
-            <li class="colorlib-active"><a href="<?php echo site_url();?>/home">Home</a></li>
-            <li><a href="<?php echo site_url();?>/consulting-services">Consulting Services</a></li>
-            <li><a href="#">Embedded Jobs </a></li>
-            <li>
-                <a href="https://iotbyanurag.github.io/Anurag-Portfolio/">About</a>
-            </li>
-            <li><a href="contact.html">Contact</a></li>
-        </ul>
-    </nav>
-</aside>
 
 <div id="primary" class="content-area col-md-8">
 	<main id="main" class="site-main" role="main">
